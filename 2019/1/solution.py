e36473af8e4d74984a542cf35bda4cbd6573f268
Andrew Dranse <adranse@sonatype.com>
2019-12-01 14:27:48 -0700

    Day 1, 2019

---
+++ 2019/1/solution.py
@@ -0,0 +1,19 @@
+lines = []
+with open('input') as f:
+    lines = f.read().splitlines()
+
+print('part 1');
+sum = 0
+for mass in lines:
+    fuel = int(int(mass) / 3) - 2
+    sum += fuel
+print(sum)
+
+print('part 2');
+sum = 0
+for mass in lines:
+    fuel = int(int(mass) / 3) - 2
+    while fuel > 0:
+        sum += fuel
+        fuel = int(fuel / 3) - 2
+print(sum)
