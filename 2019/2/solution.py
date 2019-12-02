5f290453a91bde5f57ea1952698544274d1a21c2
Andrew Dranse <adranse@sonatype.com>
2019-12-02 16:11:26 -0700

    cleanup

--- 2019/2/solution.py
+++ 2019/2/solution.py
@@ -1,21 +1,30 @@
-def run(code):
+def run(memory):
     idx = 0
     while True:
-        opcode = code[idx]
-        if opcode == 1:
-            operand1 = code[code[idx+1]]
-            operand2 = code[code[idx+2]]
-            code[code[idx+3]] = operand1 + operand2
-        elif opcode == 2:
-            operand1 = code[code[idx+1]]
-            operand2 = code[code[idx+2]]
-            code[code[idx+3]] = operand1 * operand2
-        elif opcode == 99:
-            return code
+        opcode = memory[idx]
+        if opcode == 1:  # add
+            memory[memory[idx+3]] = memory[memory[idx+1]] + memory[memory[idx+2]]
+            idx += 4
+        elif opcode == 2:  # mult
+            memory[memory[idx+3]] = memory[memory[idx+1]] * memory[memory[idx+2]]
+            idx += 4
+        elif opcode == 99:  # exit
+            return memory
         else:
             print(f'Error, unknown opcode: {opcode}')
             raise
-        idx += 4
+
+
+def run2(code):
+    for noun in range(0, 99):
+        for verb in range(0, 99):
+            test = code.copy()
+            test[1] = noun
+            test[2] = verb
+            run(test)
+            if test[0] == 19690720:
+                print(f'match: noun {noun} verb {verb} = {100 * noun + verb}')
+                return
 
 
 # test = [[1, 0, 0, 0, 99],  [2, 3, 0, 3, 99],  [2, 4, 4, 5, 99, 0],  [1, 1, 1, 4, 99, 5, 6, 0, 99]]
@@ -23,21 +32,13 @@ def run(code):
 #     print(f'{case} gives:')
 #     run(case)
 #     print(f'{case}')
-code = None
+memory = None
 with open('input') as f:
-    code = [int(x.strip()) for x in f.read().split(',')]
-print(f'input: {code}')
-part1 = code.copy()
+    memory = [int(x.strip()) for x in f.read().split(',')]
+part1 = memory.copy()
 part1[1] = 12
 part1[2] = 2
 run(part1)
-print(f'output: {part1}')
+print(f'{part1}')
 
-for noun in range(0, 99):
-    for verb in range(0, 99):
-        test = code.copy()
-        test[1] = noun
-        test[2] = verb
-        run(test)
-        if test[0] == 19690720:
-            print(f'match: noun {noun} verb {verb} {100 * noun + verb}')
+run2(memory)
