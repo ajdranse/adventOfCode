6f24a9c031e84e009001adc59b6870786c224863
Andrew Dranse <adranse@sonatype.com>
2019-12-03 09:56:31 -0700

    Simplify

--- 2019/3/solution.py
+++ 2019/3/solution.py
@@ -1,66 +1,58 @@
-lines = []
+def points(wire):
+    points = {}
+    x = 0
+    y = 0
+    steps = 0
+    for step in wire:
+        direction = step[0]
+        distance = int(step[1:])
+        if direction == 'R':
+            for _ in range(distance):
+                x += 1
+                steps += 1
+                if (x, y) not in points:
+                    points[(x, y)] = steps
+        elif direction == 'L':
+            for _ in range(distance):
+                x -= 1
+                steps += 1
+                if (x, y) not in points:
+                    points[(x, y)] = steps
+        elif direction == 'U':
+            for _ in range(distance):
+                y += 1
+                steps += 1
+                if (x, y) not in points:
+                    points[(x, y)] = steps
+        elif direction == 'D':
+            for _ in range(distance):
+                y -= 1
+                steps += 1
+                if (x, y) not in points:
+                    points[(x, y)] = steps
+    return points
+
+
+wires = []
 with open('input') as f:
-    lines = [x.split(',') for x in f.read().splitlines()]
+    wires = [x.split(',') for x in f.read().splitlines()]
 
-def update(coord, step):
-    direction = step[0]
-    distance = int(step[1:])
-    new_coords = []
-    if direction == 'R':
-        new_coords = {(coord[0] + x, coord[1], coord[2] + abs(x)) for x in range(0, distance + 1)}
-        last_coord = (coord[0] + distance, coord[1], coord[2] + abs(distance))
-    elif direction == 'U':
-        new_coords = {(coord[0], coord[1] + x, coord[2] + abs(x)) for x in range(0, distance + 1)}
-        last_coord = (coord[0], coord[1] + distance, coord[2] + abs(distance))
-    elif direction == 'D':
-        new_coords = {(coord[0], coord[1] - x, coord[2] + abs(x)) for x in range(0, distance + 1)}
-        last_coord = (coord[0], coord[1] - distance, coord[2] + abs(distance))
-    elif direction == 'L':
-        new_coords = {(coord[0] - x, coord[1], coord[2] + abs(x)) for x in range(0, distance + 1)}
-        last_coord = (coord[0] - distance, coord[1], coord[2] + abs(distance))
-    return (new_coords, last_coord)
+points1 = points(wires[0])
+points2 = points(wires[1])
+crossings = [p for p in points1.keys() if p in points2.keys()]
+print('Crossings: {}'.format(crossings))
+# min_dist = None
+# min_crossing = None
+# for c in crossings:
+#     dist = abs(c[0]) + abs(c[1])
+#     if min_dist is None or dist < min_dist:
+#         min_dist = dist
+#         min_crossing = c
+#         print('New min, crossing {} has distance {}'.format(c, dist))
+distances = [abs(p[0]) + abs(p[1]) for p in crossings]
+min_dist = min(distances)
+print('part 1: {}'.format(min_dist))
 
-
-def find_crossings(wire1, wire2):
-    wire1_nodist = {(val[0], val[1]) for val in wire1}
-    wire2_nodist = {(val[0], val[1]) for val in wire2}
-    return [val for val in wire1_nodist if val in wire2_nodist]
-
-
-def find_smallest_crossing(wire1, wire2, by_steps):
-    crossings = find_crossings(wire1, wire2)
-    min_distance = 0
-    min_crossing = (0, 0)
-    for crossing in crossings:
-        if crossing != (0,0):
-            if by_steps:
-                wire1_crossing = [val for val in wire1 if crossing[0] == val[0] and crossing[1] == val[1]][0]
-                wire2_crossing = [val for val in wire2 if crossing[0] == val[0] and crossing[1] == val[1]][0]
-                distance = wire1_crossing[2] + wire2_crossing[2]
-            else:
-                distance = abs(crossing[0]) + abs(crossing[1])
-            if min_distance == 0 or distance < min_distance:
-                min_distance = distance
-                min_crossing = crossing
-    return (min_crossing, min_distance)
-
-
-def run(wires, by_steps):
-    all_coords = []
-    for wire in wires:
-        last_coord = (0, 0, 0)
-        coords = set()
-        for step in wire:
-            (new_coords, last_coord) = update(last_coord, step)
-            coords.update(new_coords)
-        all_coords.append(coords)
-    return find_smallest_crossing(all_coords[0], all_coords[1], by_steps)
-
-
-print('part 1')
-(crossing, dist) = run(lines, False)
-print('Min crossing is {} with distance {}'.format(crossing, dist))
-
-print('part 2')
-(crossing, dist) = run(lines, True)
-print('Min crossing is {} with distance {}'.format(crossing, dist))
+steps = [points1[p] + points2[p] for p in crossings]
+min_steps = min(steps)
+print('part 2: {}'.format(min_steps))
