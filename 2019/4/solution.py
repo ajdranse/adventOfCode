670f6c9eec558d2097e4253ff3aed1f5d10b5787
Andrew Dranse <adranse@sonatype.com>
2019-12-03 22:15:04 -0700

    Day 4, 2019

---
+++ 2019/4/solution.py
@@ -0,0 +1,30 @@
+input_low = 168630
+input_high = 718098
+
+part1 = []
+part2 = []
+for x in range(input_low, input_high+1):
+    # two adjacent are the same
+    # never decrease
+    # not all repeated digits are repeated more than twice
+    digits = [int(d) for d in str(x)]
+    increasing = True
+    doubles = {}
+    for i in range(0, 5):
+        if digits[i] > digits[i+1]:
+            increasing = False
+            break
+        if digits[i] == digits[i+1]:
+            if digits[i] not in doubles:
+                doubles[digits[i]] = 2
+            else:
+                doubles[digits[i]] += 1
+
+    if increasing and len(doubles) > 0:
+        part1.append(x)
+
+    if increasing and 2 in doubles.values():
+        part2.append(x)
+
+print(len(part1))
+print(len(part2))
