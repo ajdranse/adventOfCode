e6c2c545584172fc45742425e0f380178d942827
Andrew Dranse <adranse@sonatype.com>
2019-12-06 23:32:46 -0700

    refactor

--- 2019/7/solution.py
+++ 2019/7/solution.py
@@ -3,66 +3,100 @@ from queue import Queue
 from threading import Thread
 
 
-def run(memory, i, input_queue, output_queue):
-    ip = 0
-    while True:
-        opcode = memory[ip]
-        modes = []
-        if opcode > 100:
-            raw_modes = int(opcode / 100)
-            opcode = opcode % 100
-            while raw_modes > 0:
-                modes.append(raw_modes % 10)
-                raw_modes = int(raw_modes / 10)
-        while len(modes) < 4:
-            modes.append(0)
+class IntCode(Thread):
+    def __init__(self, memory, input_queue, output_queue):
+        super(IntCode, self).__init__()
+        self.memory = memory
+        self.opcode = 0
+        self.modes = []
+        self.ip = 0
+        self.input_queue = input_queue
+        self.output_queue = output_queue
 
-        if opcode == 1:  # add
-            first = memory[ip+1] if modes[0] else memory[memory[ip+1]]
-            second = memory[ip+2] if modes[1] else memory[memory[ip+2]]
-            memory[memory[ip+3]] = first + second
-            ip += 4
-        elif opcode == 2:  # mult
-            first = memory[ip+1] if modes[0] else memory[memory[ip+1]]
-            second = memory[ip+2] if modes[1] else memory[memory[ip+2]]
-            memory[memory[ip+3]] = first * second
-            ip += 4
-        elif opcode == 3:  # input
-            memory[memory[ip+1]] = input_queue.get(True)
-            ip += 2
-        elif opcode == 4:  # output
-            first = ip+1 if modes[0] else memory[ip+1]
-            output_queue.put(memory[first])
-            ip += 2
-        elif opcode == 5:  # jump-if-true
-            first = memory[ip+1] if modes[0] else memory[memory[ip+1]]
-            second = memory[ip+2] if modes[1] else memory[memory[ip+2]]
-            if first != 0:
-                ip = second
-            else:
-                ip += 3
-        elif opcode == 6:  # jump-if-false
-            first = memory[ip+1] if modes[0] else memory[memory[ip+1]]
-            second = memory[ip+2] if modes[1] else memory[memory[ip+2]]
-            if first == 0:
-                ip = second
-            else:
-                ip += 3
-        elif opcode == 7:  # less-than
-            first = memory[ip+1] if modes[0] else memory[memory[ip+1]]
-            second = memory[ip+2] if modes[1] else memory[memory[ip+2]]
-            memory[memory[ip+3]] = 1 if first < second else 0
-            ip += 4
-        elif opcode == 8:  # equals
-            first = memory[ip+1] if modes[0] else memory[memory[ip+1]]
-            second = memory[ip+2] if modes[1] else memory[memory[ip+2]]
-            memory[memory[ip+3]] = 1 if first == second else 0
-            ip += 4
-        elif opcode == 99:  # exit
-            return
+    def parse_opcode(self):
+        self.modes = []
+        if self.opcode > 100:
+            raw_modes = int(self.opcode / 100)
+            self.opcode = self.opcode % 100
+            while raw_modes > 0:
+                self.modes.append(raw_modes % 10)
+                raw_modes = int(raw_modes / 10)
+        while len(self.modes) < 4:
+            self.modes.append(0)
+
+    def add(self):
+        first = self.memory[self.ip+1] if self.modes[0] else self.memory[self.memory[self.ip+1]]
+        second = self.memory[self.ip+2] if self.modes[1] else self.memory[self.memory[self.ip+2]]
+        self.memory[self.memory[self.ip+3]] = first + second
+        self.ip += 4
+
+    def mult(self):
+        first = self.memory[self.ip+1] if self.modes[0] else self.memory[self.memory[self.ip+1]]
+        second = self.memory[self.ip+2] if self.modes[1] else self.memory[self.memory[self.ip+2]]
+        self.memory[self.memory[self.ip+3]] = first * second
+        self.ip += 4
+
+    def input(self):
+        self.memory[self.memory[self.ip+1]] = self.input_queue.get()
+        self.ip += 2
+
+    def output(self):
+        first = self.ip+1 if self.modes[0] else self.memory[self.ip+1]
+        self.output_queue.put(self.memory[first])
+        self.ip += 2
+
+    def jump_if_true(self):
+        first = self.memory[self.ip+1] if self.modes[0] else self.memory[self.memory[self.ip+1]]
+        second = self.memory[self.ip+2] if self.modes[1] else self.memory[self.memory[self.ip+2]]
+        if first != 0:
+            self.ip = second
         else:
-            print('Error, unknown opcode: {}'.format(opcode))
-            raise
+            self.ip += 3
+
+    def jump_if_false(self):
+        first = self.memory[self.ip+1] if self.modes[0] else self.memory[self.memory[self.ip+1]]
+        second = self.memory[self.ip+2] if self.modes[1] else self.memory[self.memory[self.ip+2]]
+        if first == 0:
+            self.ip = second
+        else:
+            self.ip += 3
+
+    def lt(self):
+        first = self.memory[self.ip+1] if self.modes[0] else self.memory[self.memory[self.ip+1]]
+        second = self.memory[self.ip+2] if self.modes[1] else self.memory[self.memory[self.ip+2]]
+        self.memory[self.memory[self.ip+3]] = 1 if first < second else 0
+        self.ip += 4
+
+    def eq(self):
+        first = self.memory[self.ip+1] if self.modes[0] else self.memory[self.memory[self.ip+1]]
+        second = self.memory[self.ip+2] if self.modes[1] else self.memory[self.memory[self.ip+2]]
+        self.memory[self.memory[self.ip+3]] = 1 if first == second else 0
+        self.ip += 4
+
+    def run(self):
+        self.opcode = self.memory[self.ip]
+        while self.opcode != 99:
+            self.parse_opcode()
+            if self.opcode == 1:  # add
+                self.add()
+            elif self.opcode == 2:  # mult
+                self.mult()
+            elif self.opcode == 3:  # input
+                self.input()
+            elif self.opcode == 4:  # output
+                self.output()
+            elif self.opcode == 5:  # jump-if-true
+                self.jump_if_true()
+            elif self.opcode == 6:  # jump-if-false
+                self.jump_if_false()
+            elif self.opcode == 7:  # less-than
+                self.lt()
+            elif self.opcode == 8:  # equals
+                self.eq()
+            else:
+                print('Error, unknown opcode: {}'.format(self.opcode))
+                break
+            self.opcode = self.memory[self.ip]
 
 
 def part1(memory):
@@ -75,7 +109,7 @@ def part1(memory):
         for i in order:
             input_queue.put(i)
             input_queue.put(signal)
-            cur_thread = Thread(target=run, args=(memory.copy(), i, input_queue, output_queue))
+            cur_thread = IntCode(memory.copy(), input_queue, output_queue)
             cur_thread.start()
             cur_thread.join()
             signal = output_queue.get()
@@ -95,7 +129,7 @@ def part2(memory):
         threads = []
         for idx, phase in enumerate(order):
             queues[idx].put(phase)
-            cur_thread = Thread(target=run, args=(memory.copy(), idx, queues[idx], queues[(idx + 1) % 5]))
+            cur_thread = IntCode(memory.copy(), queues[idx], queues[(idx + 1) % 5])
             threads.append(cur_thread)
             cur_thread.start()
 
