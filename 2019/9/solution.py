2af6f6ae3c1aa054fc635011d23278a8a137cae6
Andrew Dranse <adranse@sonatype.com>
2019-12-09 18:53:34 -0700

    Finish day 9, 2019

--- 2019/9/solution.py
+++ 2019/9/solution.py
@@ -22,6 +22,10 @@ class IntCode(Thread):
 
     def set(self, idx, val):
         if idx < 0:
+            print(self.opcode)
+            print(self.modes)
+            print(memory[self.ip:self.ip+10])
+            print(val)
             raise Exception('Accessing negative index in memory: {}'.format(idx))
         if idx >= len(self.memory):
             self.memory += [0] * (idx - len(self.memory) + 1)
@@ -53,18 +57,19 @@ class IntCode(Thread):
         return ret
 
     def add(self):
-        (first, second) = self.get_addresses(2)
-        third = self.get(self.ip+3)
+        (first, second, third) = self.get_addresses(3)
         self.set(third, self.get(first) + self.get(second))
         self.ip += 4
 
     def mult(self):
-        (first, second) = self.get_addresses(2)
-        self.set(self.get(self.ip+3), self.get(first) * self.get(second))
+        (first, second, third) = self.get_addresses(3)
+        self.set(third, self.get(first) * self.get(second))
         self.ip += 4
 
     def input(self):
-        self.set(self.get(self.ip+1), self.input_queue.get())
+        first = self.get_addresses(1)[0]
+        val = self.input_queue.get()
+        self.set(first, val)
         self.ip += 2
 
     def output(self):
@@ -87,13 +92,13 @@ class IntCode(Thread):
             self.ip += 3
 
     def lt(self):
-        (first, second) = self.get_addresses(2)
-        self.set(self.get(self.ip+3), 1 if self.get(first) < self.get(second) else 0)
+        (first, second, third) = self.get_addresses(3)
+        self.set(third, 1 if self.get(first) < self.get(second) else 0)
         self.ip += 4
 
     def eq(self):
-        (first, second) = self.get_addresses(2)
-        self.set(self.get(self.ip+3), 1 if self.get(first) == self.get(second) else 0)
+        (first, second, third) = self.get_addresses(3)
+        self.set(third, 1 if self.get(first) == self.get(second) else 0)
         self.ip += 4
 
     def update_relative_base(self):
@@ -129,65 +134,43 @@ class IntCode(Thread):
             self.opcode = self.get(self.ip)
 
 
-test = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
-q = Queue()
-t = IntCode(test.copy(), None, q)
-t.start()
-t.join()
+tests = [
+    [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99],
+    [1102, 34915192, 34915192, 7, 4, 7, 99, 0],
+    [104, 1125899906842624, 99]
+]
 
-while not q.empty():
-    print(q.get())
+for test in tests:
+    q = Queue()
+    t = IntCode(test.copy(), None, q)
+    t.start()
+    t.join()
 
-# def part1(memory):
-#     max_signal = 0
-#     max_order = []
-#     for order in itertools.permutations([0, 1, 2, 3, 4], 5):
-#         signal = 0
-#         input_queue = Queue()
-#         output_queue = Queue()
-#         for i in order:
-#             input_queue.put(i)
-#             input_queue.put(signal)
-#             cur_thread = IntCode(memory.copy(), input_queue, output_queue)
-#             cur_thread.start()
-#             cur_thread.join()
-#             signal = output_queue.get()
-#
-#         if signal > max_signal:
-#             max_signal = signal
-#             max_order = order
-#     print(max_order, max_signal)
-#
-#
-# def part2(memory):
-#     max_signal = 0
-#     max_order = []
-#     for order in itertools.permutations([5, 6, 7, 8, 9], 5):
-#         signal = 0
-#         queues = [Queue() for _ in range(5)]
-#         threads = []
-#         for idx, phase in enumerate(order):
-#             queues[idx].put(phase)
-#             cur_thread = IntCode(memory.copy(), queues[idx], queues[(idx + 1) % 5])
-#             threads.append(cur_thread)
-#             cur_thread.start()
-#
-#         queues[0].put(0)
-#         for t in threads:
-#             t.join()
-#
-#         signal = queues[0].get()
-#
-#         if signal > max_signal:
-#             max_signal = signal
-#             max_order = order
-#     print(max_order, max_signal)
-#
-#
-# with open('input') as f:
-#     memory = [int(x.strip()) for x in f.read().split(',')]
-#     print('part 1')
-#     part1(memory.copy())
-#
-#     print('part 2')
-#     part2(memory.copy())
+    output = []
+    while not q.empty():
+        output.append(str(q.get()))
+    print(','.join(output))
+
+with open('input') as f:
+    memory = [int(x.strip()) for x in f.read().split(',')]
+    print('part 1')
+    inq = Queue()
+    outq = Queue()
+    t = IntCode(memory.copy(), inq, outq)
+    t.start()
+    inq.put(1)
+    t.join()
+    output = []
+    while not outq.empty():
+        output.append(str(outq.get()))
+    print(','.join(output))
+
+    print('part 2')
+    t = IntCode(memory.copy(), inq, outq)
+    t.start()
+    inq.put(2)
+    t.join()
+    output = []
+    while not outq.empty():
+        output.append(str(outq.get()))
+    print(','.join(output))
