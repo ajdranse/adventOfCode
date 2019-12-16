3e4cb4e11b6c0da4517d245d0343000e941345a4
Andrew Dranse <adranse@sonatype.com>
2019-12-16 11:06:13 -0700

    Part 2 of Day 16, 2019

--- 2019/16.py
+++ 2019/16.py
@@ -7,6 +7,7 @@ test2 = '80871224585914546619083218645595'
 test3 = '19617804207202209144916044189917'
 test4 = '69317163492948606335995924319873'
 
+
 def fft(inp):
     final_output = ''
     for i in range(100):
@@ -29,14 +30,32 @@ def fft(inp):
         output = ''
     print(final_output)
 
+
 # fft(test2)
 # fft(test3)
 # fft(test4)
 with open('16.in') as f:
     inp = f.readline().strip()
-    # print('part1')
-    # fft(inp)
+    print('part1')
+    fft(inp)
 
-    part2inp = inp * 10000
-    offset = int(part2inp[:7])
-    print(offset)
+with open('16.in') as f:
+    input_string = f.read().strip()
+
+    # get offset to read
+    offset = int(input_string[:7])
+    # repeat list
+    part2 = list(map(int, input_string)) * 10000
+    # do fft 100 times
+    for i in range(100):
+        # get sum of everything after offset
+        sum_of_all_after_offset = sum(part2[j] for j in range(offset, len(part2)))
+        # for each position in the output that we care about
+        for j in range(offset, len(part2)):
+            # copy current partial sum
+            partial = sum_of_all_after_offset
+            # remove the value of current character from sum
+            sum_of_all_after_offset -= part2[j]
+            # write output sum
+            part2[j] = abs(partial) % 10
+    print(part2[offset:offset+8])
