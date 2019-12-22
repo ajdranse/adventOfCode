8cc0b0d983b105678ae48a7fe77b65f6f2d461a3
Andrew Dranse <adranse@sonatype.com>
2019-12-21 19:02:59 -0700

    Day 21 part 2, 2019

--- 2019/21.py
+++ 2019/21.py
@@ -1,15 +1,16 @@
-from intcode import print_grid, IntCode
+from intcode import IntCode
 from queue import Queue
 
 
 def from_ascii(vals):
-    try :
-        out = ''
-        for c in vals:
+    out = ''
+    for c in vals:
+        try:
             out += str(chr(c))
-        return out
-    except ValueError:
-        print('Not ascii: {}'.format(vals))
+        except ValueError:
+            print('Not ascii: {}'.format(c))
+    return out
+
 
 def to_ascii(instr):
     out = []
@@ -20,6 +21,45 @@ def to_ascii(instr):
     return out
 
 
+def part1(inq):
+    # true if there is a hole at 1
+    for v in to_ascii('NOT A T'):
+        inq.put(v)
+    # true if there is a hole at 2
+    for v in to_ascii('NOT B J'):
+        inq.put(v)
+    # true if htere is a hole at 1 or 2
+    for v in to_ascii('OR T J'):
+        inq.put(v)
+    # true if there is a hole at 3
+    for v in to_ascii('NOT C T'):
+        inq.put(v)
+    # true if there is a hole at 1, 2, or 3
+    for v in to_ascii('OR T J'):
+        inq.put(v)
+    # true if there is ground at 4
+    for v in to_ascii('AND D J'):
+        inq.put(v)
+
+
+def part2(inq):
+    # J will be set to true if there is a hole at 1, 2, or 3 and ground at 4
+    # we want to NOT jump if there is a hole at 5 or 8 (E or H)
+    # so E or H must be true (ground)
+    # reset T to True (D must be true)
+    for v in to_ascii('OR D T'):
+        inq.put(v)
+    # put E in T
+    for v in to_ascii('AND E T'):
+        inq.put(v)
+    # E or H is true
+    for v in to_ascii('OR H T'):
+        inq.put(v)
+    # put result in jump
+    for v in to_ascii('AND T J'):
+        inq.put(v)
+
+
 print('part1')
 with open('21.in') as f:
     memory = [int(x.strip()) for x in f.read().split(',')]
@@ -27,31 +67,35 @@ with open('21.in') as f:
     outq = Queue()
     t = IntCode(memory.copy(), inq, outq)
     t.start()
-# part 1
-# jump if there is ground at 4 and a hole at 1 2 or 3
-# NOT A T # T is true if there is a hole at 1
-# NOT B J # J is true if there is a hole at 2
-# OR T J # J is true if there is a hole at 1 or 2
-# NOT C T # J is true if there is a hole at 3
-# OR T J # T is true if htere is a hole at 1 2 or 3
-# AND D J # J is true if there is ground at 4 and a hole at 1 2 or 3
     vals = []
     while not outq.empty():
         vals.append(outq.get())
     print(from_ascii(vals))
+    part1(inq)
+    # go
+    for v in to_ascii('WALK'):
+        inq.put(v)
+    t.join()
+    while not outq.empty():
+        vals.append(outq.get())
+    print(from_ascii(vals))
 
-    for v in to_ascii('NOT A T'):
-        inq.put(v)
-    for v in to_ascii('NOT B J'):
-        inq.put(v)
-    for v in to_ascii('OR T J'):
-        inq.put(v)
-    for v in to_ascii('NOT C T'):
-        inq.put(v)
-    for v in to_ascii('OR T J'):
-        inq.put(v)
-    for v in to_ascii('AND D J'):
-        inq.put(v)
+print('part2')
+with open('21.in') as f:
+    memory = [int(x.strip()) for x in f.read().split(',')]
+    inq = Queue()
+    outq = Queue()
+    t = IntCode(memory.copy(), inq, outq)
+    t.start()
+    vals = []
+    while not outq.empty():
+        vals.append(outq.get())
+    print(from_ascii(vals))
+    # jump if there is a hole at 1, 2, or 3, and ground at 4
+    part1(inq)
+    # don't jump if there is a hole at 5 or 8 (i.e. we can't walk a step or jump once more)
+    part2(inq)
+    # go
     for v in to_ascii('RUN'):
         inq.put(v)
     t.join()
