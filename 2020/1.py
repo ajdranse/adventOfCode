25b1fc2baf15273b566b6ac719bd53769a5cc8c7
Andrew Dranse <adranse@sonatype.com>
2020-12-01 10:16:32 -0700

    combinations doesn't allow duplicate tuples, it's better

--- 2020/1.py
+++ 2020/1.py
@@ -5,13 +5,13 @@ with open('1.in') as f:
     lines = [int(x) for x in f.read().splitlines()]
 
 print('part 1')
-for i, j in itertools.permutations(lines, 2):
+for i, j in itertools.combinations(lines, 2):
     if i + j == 2020:
         print(i, j, i*j)
         break
 
 print('part 2')
-for i, j, k in itertools.permutations(lines, 3):
+for i, j, k in itertools.combinations(lines, 3):
     if i + j + k == 2020:
         print(i, j, k, i*j*k)
         break
