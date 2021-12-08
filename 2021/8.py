0d849f65e5c65b1d7803dd6251b05315e0257a1d
Andrew Dranse <andrew.dranse@elevatelabs.com>
2021-12-08 11:16:14 -0700

    refactor

--- 2021/8.py
+++ 2021/8.py
@@ -1,29 +1,12 @@
 from collections import defaultdict
 
 
-lines = [
-'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
-]
-
-lines = [
-'be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe',
-'edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc',
-'fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg',
-'fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb',
-'aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea',
-'fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb',
-'dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe',
-'bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef',
-'egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb',
-'gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce',
-]
-
 with open('8.in') as f:
     lines = f.read().splitlines()
 
 num_unique = 0
-for l in lines:
-    (signal, output) = l.split(' | ')
+for line in lines:
+    (signal, output) = line.split(' | ')
     for w in output.split():
         if len(w) == 2 or len(w) == 3 or len(w) == 4 or len(w) == 7:
             num_unique += 1
@@ -31,14 +14,15 @@ print('part 1:', num_unique)
 
 total_total = 0
 segments = defaultdict(str)
-for l in lines:
-    (signal, output) = l.split(' | ')
+for line in lines:
+    (signal, output) = line.split(' | ')
     signals = signal.split()
+
     len_to_signal = defaultdict(list)
+    to_num = defaultdict(int)
+
     for s in signals:
         len_to_signal[len(s)].append(s)
-
-    to_num = defaultdict(int)
 
     to_num[len_to_signal[2][0]] = 1
     to_num[len_to_signal[3][0]] = 7
@@ -51,12 +35,12 @@ for l in lines:
     num7 = len_to_signal[3][0]
     for c in num1:
         num7 = num7.replace(c, '')
-    segments['a']= num7
+    segments['a'] = num7
 
     # figure out 3
     # one sharing c and f with 1
     num3 = None
-    cs = list(num1)
+    cs = num1
     for w in len_to_signal[5]:
         if all([c in w for c in cs]):
             to_num[w] = 3
@@ -66,14 +50,14 @@ for l in lines:
     # figure out b
     # character in 4 that's not in 3
     num4 = len_to_signal[4][0]
-    for c in list(num4):
+    for c in num4:
         if c not in num3:
             segments['b'] = c
             break
 
     # figure out d
     # character in 4 that isn't b and isn't in 1
-    for c in list(num4):
+    for c in num4:
         if c != segments['b'] and c not in num1:
             segments['d'] = c
             break
@@ -82,12 +66,12 @@ for l in lines:
     # contains b, d, and one character from 1
     for w in len_to_signal[5]:
         if segments['b'] in w and segments['d'] in w:
-            matched = [c in w for c in list(num1)]
+            matched = [c in w for c in num1]
             if any(matched) and not all(matched):
                 to_num[w] = 5
                 # figure out f (the shared char with 1)
                 # figure out c (the other char in 1)
-                for c in list(num1):
+                for c in num1:
                     if c in w:
                         segments['f'] = c
                     else:
@@ -105,7 +89,7 @@ for l in lines:
     # we know d and c, so the len6 that's missing a char that's not d or c is 9 and that char is e
     num8 = len_to_signal[7][0]
     for w in len_to_signal[6]:
-        for c in list(num8):
+        for c in num8:
             if c not in w:
                 if c == segments['d']:
                     to_num[w] = 0
